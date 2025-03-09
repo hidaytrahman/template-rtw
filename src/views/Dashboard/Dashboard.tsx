01765b74468d00f74927f06eddb99980e594887f
Hidayt Rahman <hidaytrahman@gmail.com>
2025-03-09 21:30:22 +0530

    feat: nested pages setup with dashboard

---
+++ src/views/Dashboard/Dashboard.tsx
@@ -0,0 +1,27 @@
+import { Box, Container, Grid2 as Grid, Typography } from "@mui/material";
+import { NavLink, Outlet } from "react-router";
+
+export default function Dashboard() {
+	return (
+		<Container maxWidth="lg">
+			<Box p={2}>
+				<Typography variant="h1">Dashboard</Typography>
+			</Box>
+
+			<Grid container spacing={2}>
+				<Grid size={4} sx={{ background: "#f3f3f3" }} p={2} minHeight={400}>
+					{/* Navigation for the dashboard */}
+					<Box display={"flex"} flexDirection={"column"}>
+						<NavLink to="/dashboard">Home</NavLink>
+						<NavLink to="/dashboard/settings">Settings</NavLink>
+					</Box>
+				</Grid>
+				<Grid size={8} sx={{ background: "#f3f3f3" }} p={2}>
+					{/* will either be <Home/> or <Settings/> */}
+					<Outlet />
+				</Grid>
+
+			</Grid>
+		</Container>
+	);
+}
