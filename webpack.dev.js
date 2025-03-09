095369c96fa031996b3c19dae480fc5b298f5a59
Hidayt Rahman <hidaytrahman@gmail.com>
2025-03-10 04:22:09 +0530

    chore: utilise webpack-merge package for the common config

---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map', // For better debugging
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    historyApiFallback: true, // Handle client-side routing
+    hot: true, // Enable Hot Module Replacement (HMR)
+    compress: true,
+    port: 3000,
+  },
+});
