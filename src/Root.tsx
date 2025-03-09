84f103c677da76b264b521b3a61256432dc53990
Hidayt Rahman <hidaytrahman@gmail.com>
2025-03-09 20:15:29 +0530

    feat: root handling

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,42 +1,25 @@
 import { BrowserRouter, Link, NavLink, Outlet, Route, Routes } from 'react-router'
 import Landing from './views/Landing/Landing'
 import About from './views/About'
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import ErrorBoundary from './ErrorBoundary'
+import { Navigation } from './components'
 
 export default function Root() {
 	return (
-		<div>
-			{/* <NavLink
-				to="/"
-				className={({ isActive }) =>
-					isActive ? "active" : ""
-				}
-			>
-				Home
-			</NavLink>
+		<ErrorBoundary>
+			<>
+				<article>
+					<BrowserRouter>
+						<Navigation />
+						<Routes>
+							<Route index element={<Landing />} />
+							<Route path="about" element={<About />} />
+						</Routes>
+					</BrowserRouter>
+				</article>
 
-			<NavLink
-				to="/"
-				className={({ isActive }) =>
-					isActive ? "active" : ""
-				}
-			>
-				About
-			</NavLink> */}
-			
-			<Typography>Welcome</Typography>
-
-			<Outlet />
-
-			<BrowserRouter>
-				<Routes>
-					<Route index element={<Landing />} />
-					<Route path="about" element={<About />} />
-
-				</Routes>
-			</BrowserRouter>
-
-
-		</div>
+			</>
+		</ErrorBoundary>
 	)
 }
