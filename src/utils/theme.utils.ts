5b1b89cd54ddd827f8b64df5d84ffce20007addf
hidaytrahman <hidaytrahman@gmail.com>
2025-05-29 21:48:36 +0530

    feat: theme implementation

---
+++ src/utils/theme.utils.ts
@@ -0,0 +1,28 @@
+import { createTheme } from "@mui/material";
+
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // allow configuration using `createTheme()`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+
+export const theme = createTheme({
+   palette: {
+    primary: {
+      main: '#8A784E',
+    },
+    secondary: {
+      main: '#edf2ff',
+    },
+  },
+});
